019a93444d11a82e36ff08702ec7807f6e3f133e
Remi Rosenthal <remi-rosenthal@hotmail.fr>
2025-07-08 13:04:47 +0000

    test

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify
 
 # 1. Initialize the Flask application
+#from flask_cors import CORS # 1. Import CORS
+
 app = Flask(__name__)
+#CORS(app) # 2. Initialize CORS with your app
 
 # 2. Define the route and the function to handle it
 @app.route('/get_one')
@@ -14,4 +17,4 @@ def get_one():
 # 4. Run the app when the script is executed
 if __name__ == '__main__':
     # The app will run on http://127.0.0.1:5000
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000)
