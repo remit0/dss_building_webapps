2c7b0b9ff45bb74de07503ad71b07652d9283557
Remi Rosenthal <remi-rosenthal@hotmail.fr>
2025-07-08 11:32:57 +0000

    temp

--- my_app/src/app/app.ts
+++ my_app/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'my_app';
+
+export class App  {
+  title: String = "my-app";
+  projects: String = "";
+  backendUrl: string ='';
+
+  constructor (private http:HttpClient){}
+
+  ngOnInit() {
+    this.backendUrl = 'http://127.0.0.1:5000/get_one';
+    this.http.get<String>(this.backendUrl, { responseType: 'text' as 'json'})
+    .subscribe((resp:any) => {
+      this.projects = resp
+    });
+  }
 }
