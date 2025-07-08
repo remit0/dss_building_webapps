2d3df40b62f110594d28fa10964b1327a2184067
Remi Rosenthal <remi-rosenthal@hotmail.fr>
2025-07-08 11:11:46 +0000

    init frontend

--- frontend/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-
-// This function starts the Angular application using our main component
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
